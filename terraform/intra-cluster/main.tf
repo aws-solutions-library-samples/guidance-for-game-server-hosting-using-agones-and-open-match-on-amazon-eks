826e382f25a79cc6feaea00e851b6fd3b3c47fa9
Serge Poueme <10876571+sdpoueme@users.noreply.github.com>
2024-06-08 11:31:58 -0400

    Update main.tf
    Fixing typo in module

--- terraform/intra-cluster/main.tf
+++ terraform/intra-cluster/main.tf
@@ -97,6 +97,7 @@ module "eks_blueprints_addons" {
   enable_aws_for_fluentbit            = true
   enable_aws_load_balancer_controller = true
   aws_load_balancer_controller = {
+    set
       {
         name  = "enableServiceMutatorWebhook"
         value = "false"
