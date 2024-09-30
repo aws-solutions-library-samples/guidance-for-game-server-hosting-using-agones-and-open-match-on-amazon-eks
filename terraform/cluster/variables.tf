a98897c5740c86167f492ebf660f25219ec43734
JP Velasco <jpvelas@amazon.com>
2024-09-30 01:16:43 -0700

    Adding parameter for the optional admin role

--- terraform/cluster/variables.tf
+++ terraform/cluster/variables.tf
@@ -32,3 +32,14 @@ variable "cluster_version" {
   type    = string
   default = "1.28"
 }
+
+variable "admin_role_arn" {
+  description = "ARN of the admin role"
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = var.admin_role_arn == "" || can(regex("^arn:aws:iam::[0-9]{12}:role/.+", var.admin_role_arn))
+    error_message = "The admin_role_arn value must be a valid IAM role ARN or an empty string."
+  }
+}
